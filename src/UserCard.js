debcd4aad9908271e44831367a7cd6cbffbea997
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-20 20:43:31 +0300

    Done task

---
+++ src/UserCard.js
@@ -0,0 +1,16 @@
+export function createUserCard({name = '', description = ''}) {
+  const card = document.createElement('article');
+  const heading = document.createElement('h2');
+  heading.innerText = name;
+  const cardDescription = document.createElement('p');
+  cardDescription.innerText = description;
+  card.append(heading, cardDescription);
+  return card;
+}
+
+function renderCard(user) {
+  const rootElem = document.getElementById('root');
+  rootElem.append(createUserCard(user));
+}
+
+export default renderCard;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import lodash from 'lodash';
 import useless, {sum, sub} from './MyMath';
 import Component from './Component';
 import testFile, * as testData from './test';
+import renderUserCard from './UserCard';
 import './styles.css';
 
 console.log(testFile);
@@ -16,6 +17,12 @@ component.render();
 console.log(lodash.random(0,200));
 console.log('test');
 
+const user = {
+  name: 'John Smith',
+  description: 'lorem123',
+}
+
+renderUserCard(user);
 
 /*
   Task
